e052b3ff18d271ce17d63e380b84675cc8d1878b
Akash8377 <121550790+Akash8377@users.noreply.github.com>
2024-03-12 11:54:29 +0530

    Add files via upload

---
+++ twoSets.js
@@ -0,0 +1,12 @@
+function getTotalX(a, b) {
+    let both = a.concat(b)
+    console.log(both)
+    for(let i=0;i<a.length;i++){
+        // console.log("a data",a[i])
+        // let factors = a[i]%a
+    }
+    for(let j=0;j<b.length;j++){
+        // console.log("j data",b[j])
+    }
+}
+console.log(getTotalX([4,8],[16,32,96]))
